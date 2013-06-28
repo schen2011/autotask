5d423e4afd57cbcc73504812aef29d25d59a15f8
gbaney <gbaney@Mobile12.clv.main.asmgi.com>
2013-06-28 19:25:18 -0400

    establish the soap connection, but not able to call the query method. will read the debug and find out the issue.

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+require_once('lib/nusoap.php');
+
+date_default_timezone_set ( 'America/New_York' );
+ini_set ( 'display_errors', 1 );
+
+ini_set ( 'log_errors', 1 );
+
+ini_set ( 'error_log', dirname ( __FILE__ ) . 'error_log.txt' );
+
+error_reporting ( E_ALL );
 //http://theninthnode.com/2010/11/autotask-getting-a-list-of-open-support-tickets-by-client-via-the-api/
 //http://theninthnode.com/2010/11/connecting-to-autotask-with-php/
 //$username = 'schenjobs@gmail.com';
@@ -14,8 +24,9 @@ function connectToAPI($username, $password){
         echo '<br>connecting to API<br>';
         $wsdl = 'https://webservices3.autotask.net/atservices/1.5/atws.wsdl';
         
-        echo file_get_contents($wsdl);
-        die();
+        //echo file_get_contents($wsdl);
+        //echo '<br>';
+        //die();
 
         $loginarray = array
             (
@@ -24,7 +35,26 @@ function connectToAPI($username, $password){
                 'uri'=>"http://autotask.net/ATWS/v1_5/",
                 'location'=>"https://webservices3.autotask.net/atservices/1.5/atws.asmx"
             );
-        $client = new SoapClient($wsdl, $loginarray);
+        
+        $client = new nusoap_client($wsdl, $loginarray);
+        
+        $err = $client->getError();
+        echo 'do we have errors?' . $err;
+        if ($err) {
+                echo '<h2>Constructor error</h2><pre>' . $err . '</pre>';
+                echo '<h2>Debug</h2><pre>' . htmlspecialchars($client->getDebug(), ENT_QUOTES) . '</pre>';
+                exit();
+        }
+
+        echo '<br>';
+        
+        //$func = $client->__getFunctions();
+        
+        
+        //print_r($func);
+        
+        echo '<br>';
+        
         return $client;
 }
     
@@ -32,22 +62,53 @@ function connectToAPI($username, $password){
 
 function getTickets(){
      $username = 'gbaney@asmgi.com';
-     $password = 'stephenchen1';
+     $password = 'stephenchen2';
      $ticketno = 'T20130623.0113';
      $client = connectToAPI($username, $password);     
+     
+     
      $xml = array('sXml' => "<queryxml>" .
              "<entity>Ticket</entity>" .
              "<query>" .
              "<field>id</field>" .
-             "<expression op='equals'>" .
+             "<expression op='equals'><field>" .
              $ticketno .
             "</expression></field></query>" .
              "</queryxml>");
      
-     $result = $client->query($xml);
-     $ticketso = $result->queryResult->EntityResults->Entity;
-     $ticketcount = count($ticketso);
-     return $ticketcount;
+     print_r ($xml);
+     
+     /*
+     $xml = array('sXml' => "<queryxml>" .
+             "<entity>Ticket</entity>" .
+             "<query>" .
+             "<field>id</field>" .
+            "</query>" .
+             "</queryxml>");
+     */
+     //$xml = "<queryxml><entity>Ticket</entity><query><field>id</field></query></queryxml>";
+     
+     $result = $client->call('query', $xml);
+     //$result = $client->query($xml);
+     //$result = $client->__soapCall('query', $xml);
+     
+    if ($client->fault) {
+            echo '<h2>Fault (Expect - The request contains an invalid SOAP body)</h2><pre>'; print_r($result); echo '</pre>';
+    } else {
+            $err = $client->getError();
+            if ($err) {
+                    echo '<h2>Error</h2><pre>' . $err . '</pre>';
+            } else {
+                    echo '<h2>Result</h2><pre>'; print_r($result); echo '</pre>';
+            }
+    }
+    echo '<h2>Request</h2><pre>' . htmlspecialchars($client->request, ENT_QUOTES) . '</pre>';
+    echo '<h2>Response</h2><pre>' . htmlspecialchars($client->response, ENT_QUOTES) . '</pre>';
+    echo '<h2>Debug</h2><pre>' . htmlspecialchars($client->getDebug(), ENT_QUOTES) . '</pre>';
+         //$ticketso = $result->queryResult->EntityResults->Entity;
+     
+     //$ticketcount = count($ticketso);
+     //return $ticketcount;
 }
 
 
